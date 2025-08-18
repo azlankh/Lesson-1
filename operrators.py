19df16c62a4b62d852252d694f5b1e0df372cf20
azlankh <azlan.k.harris@gmail.com>
2025-08-18 20:26:39 +0530

    operrators

---
+++ operrators.py
@@ -0,0 +1,13 @@
+tree_1 =int(input("Enter the the 1st height of the tree:"))
+tree_2 =int(input("Enter the the 2nd height of the tree:"))
+tree_3 =int(input("Enter the the 3rd height of the tree:"))
+tree_4 =int(input("Enter the the 4th height of the tree:"))
+tree_5 =int(input("Enter the the 5th height of the tree:"))
+
+sum = tree_1 + tree_2 + tree_3 + tree_4 + tree_5
+
+print("The Sum of all the height of the trees are:",sum)
+
+avarage = sum/5
+
+print("The Avarage Height of A Tree is:",avarage)
