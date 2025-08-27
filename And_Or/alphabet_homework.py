88e79be7a02aee95f4bef144baf542841f45c0d6
azlankh <azlan.k.harris@gmail.com>
2025-08-27 20:38:46 +0530

    alphabet homework

---
+++ And_Or/alphabet_homework.py
@@ -0,0 +1,17 @@
+coding = input("Whats the missing alphabet in 'codin_'? ")
+a = 'g' 
+if coding == a:
+    print("Correct")
+if not coding == a:
+    print("Incorrect")
+    print("The correct answer is 'g'")
+mouse = input("Whats the missing alphabet in 'mou_e'? ")
+b = 's'
+if mouse == b:
+    print("Correct")
+if not mouse == b:
+    print("Incorrect")
+    print("The correct answer is 's'")
+    print("Well done for trying!")
+
+
