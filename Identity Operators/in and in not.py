134b890b4a574f51769342c9fafe0a977e5490ae
azlankh <azlan.k.harris@gmail.com>
2025-08-28 21:31:05 +0530

    in and in not

---
+++ Identity Operators/in and in not.py
@@ -0,0 +1,10 @@
+print("Membership Operators:")
+a = input("Enter a number between 1-10: ")
+b = ("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+if a in b:
+    print("Your Number Is Between 1-10")
+else:
+    print("Your Number Is Not Between 1-10")
+
+
+    
