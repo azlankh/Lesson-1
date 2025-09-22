8226fdbe10fddb18ce41ed46d23b329408ee57fc
azlankh <azlan.k.harris@gmail.com>
2025-09-22 20:54:12 +0530

    Add files via upload

---
+++ ride opinion.py
@@ -0,0 +1,14 @@
+ride = input("Whick Ride do you Want to Go on?? (A Bike Ride / A Car Ride): ")
+if ride == "A Bike Ride" or ride == "a bike ride":
+    opinion_bike = input("Which Bike do you Want to Ride?? (A Mountain Bike / A Scooter): ")
+    if opinion_bike == "A Mountain Bike" or opinion_bike == "a mountain bike":
+        print("You Chose A Mountain Bike, Enjoy Your Ride!!")
+    elif opinion_bike == "A Scooter" or opinion_bike == "a scooter":
+        print("You Chose A Scooter, Enjoy Your Ride!!")
+elif ride == "A Car Ride" or ride == "a car ride":
+    opinion_car = input("Which Car do you Want to Ride?? (A Family Car / A Normal Car): ")
+    if opinion_car == "A Family Car" or opinion_car == "a family car":
+        print("You Chose A Family Car, Enjoy Your Ride!!")
+    elif opinion_car == "A Normal Car" or opinion_car == "a normal car":
+        print("You Chose A Normal Car, Enjoy Your Ride!!")
+  
