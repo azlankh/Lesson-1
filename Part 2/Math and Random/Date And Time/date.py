892d8b203e46f8b12806e6d97015d7e462496b4d
azlankh <azlan.k.harris@gmail.com>
2025-11-20 21:51:12 +0530

    20-11-2025

---
+++ Part 2/Math and Random/Date And Time/date.py
@@ -0,0 +1,8 @@
+import datetime
+
+today = datetime.date.today()
+now = datetime.datetime.now()
+
+print("Todays Date:", today)
+print("Current Time:", now)
+print("Date Components:", today.day, today.month, today.year)
