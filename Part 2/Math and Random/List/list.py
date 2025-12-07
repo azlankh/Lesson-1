53abaec91ce82fb6015c77f5b931d221d5669ec9
azlankh <azlan.k.harris@gmail.com>
2025-12-07 14:17:20 +0530

    7-12-2025

---
+++ Part 2/Math and Random/List/list.py
@@ -0,0 +1,9 @@
+# create lists
+
+numbers = [10, 20, 30, 40]
+
+mixed = [1, "hello", 3.14, True]
+
+print(numbers) # [10, 20, 30, 40]
+print(mixed[1]) # 'hello' (indexing starts at 0)
+print(type(numbers)) # <class 'list'>
