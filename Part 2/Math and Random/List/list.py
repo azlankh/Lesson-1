9454aea31ff5dd3fff4d4af6b2dec9ae719f5673
azlankh <azlan.k.harris@gmail.com>
2025-12-07 14:18:21 +0530

    2-2-2-2

--- Part 2/Math and Random/List/list.py
+++ Part 2/Math and Random/List/list.py
@@ -6,4 +6,4 @@ mixed = [1, "hello", 3.14, True]
 
 print(numbers) # [10, 20, 30, 40]
 print(mixed[1]) # 'hello' (indexing starts at 0)
-print(type(numbers)) # <class 'list'>
+print(type(numbers)) # <class 'list'>      
